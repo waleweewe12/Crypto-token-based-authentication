fb0f60992a2a041937c6dabf051a94b0f6c30a80
weerapath ruenrurngdee <m17661weerapast@gmail.com>
2020-11-29 16:44:12 +0700

    test cookie

--- frontend/hello-react/src/App.js
+++ frontend/hello-react/src/App.js
@@ -5,6 +5,9 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
+
+document.cookie = "username=John Doe";
+
 function App() {
   return (
     <div >
